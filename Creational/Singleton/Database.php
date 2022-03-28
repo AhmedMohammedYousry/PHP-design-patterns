57f72d2b154b1c8ec9f4deb3481b5f9c55d89f3a
Ahmed Mohamed Yousry | Ejada Sytems Ltd <amyousry@ejadacairo.local>
2022-03-28 10:30:12 +0200

    Singleton Design Pattern

---
+++ Creational/Singleton/Database.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Creational\Singleton;
+
+class Database 
+{
+    public static $instance;
+
+    public static function getInstance()
+    {
+        if(!isset(self::$instance))
+        {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+
+    private function __construct()
+    {
+        
+    }
+
+    public function getQuery()
+    {
+        return "SELECT * FROM table";
+    }
+}
+
+$db = Database::getInstance();
+$db2 = Database::getInstance();
+$db3 = Database::getInstance();
+
+var_dump($db);
+var_dump($db2);
+var_dump($db3);
+
+echo $db->getQuery();
